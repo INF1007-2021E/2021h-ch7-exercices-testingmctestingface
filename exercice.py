ca7c8b1dfbbada300a32b27f6a05b7b91c8ac375
testingmctestingface <raphael.lheureux@polymtl.ca>
2021-03-09 13:06:19 -0500

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -2,13 +2,16 @@
 # -*- coding: utf-8 -*-
 
 # TODO: Importez vos modules ici
-
+import math
+import turtle
 
 
 # TODO: Définissez vos fonction ici
-
+def f(a = 5, b = 2, c = 1, p = 4):
+    V = (4/3) * math.pi * a * b * c
+    M = V * p
+    return (V,M)
 
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
-
-    pass
+    print(f())
